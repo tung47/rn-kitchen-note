161c0dff95b1b954f9257f5de2ecc016ac9abda1
tung47 <tung.nguyen271193@gmail.com>
2021-06-04 10:57:13 +0300

    add CategoriesScreen

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CategoriesScreen = () => {};
+
+const styles = StyleSheet.create({});
+
+export default CategoriesScreen;
