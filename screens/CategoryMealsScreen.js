1815070a9d2a41310a7a80eb518c4b31db1dc3e3
tung47 <tung.nguyen271193@gmail.com>
2021-06-11 23:32:23 +0300

    set dynamic navagation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam('categoryId');
@@ -25,6 +26,20 @@ const CategoryMealsScreen = (props) => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+  };
 };
 
 const styles = StyleSheet.create({
