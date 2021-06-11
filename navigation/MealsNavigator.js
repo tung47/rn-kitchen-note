ca8b1ed85dcc89220a374635936e3a18342abff3
tung47 <tung.nguyen271193@gmail.com>
2021-06-11 23:51:53 +0300

    set default navigation options and config

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,16 +1,27 @@
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen,
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailScreen,
   },
-  MealDetail: MealDetailScreen,
-});
+  {
+    mode: 'modal',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam('categoryId');
@@ -35,10 +34,6 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedCategory.title,
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
   };
 };
 
