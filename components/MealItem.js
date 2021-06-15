b3e79e353af9768d368efb5f968c4d34fbd8c637
tung47 <tung.nguyen271193@gmail.com>
2021-06-15 23:59:28 +0300

    meal ist

---
+++ components/MealItem.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+
+const MealItem = (props) => {
+  return (
+    <TouchableOpacity onPress={props.onSelectMeal}>
+      <View style={styles.mealItem}>
+        <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+          <ImageBackground source={{ uri: props.image }} style={styles.bgImage}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title} numberOfLines={1}>
+                {props.title}
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+        <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+          <Text>{props.duration}m</Text>
+          <Text>{props.complexity.toUpperCase()}</Text>
+          <Text>{props.affordability.toUpperCase()}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: '100%',
+    backgroundColor: '#ccc',
+  },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+  mealRow: {
+    flexDirection: 'row',
+  },
+  mealHeader: {
+    height: '90%',
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+  },
+});
+
+export default MealItem;
