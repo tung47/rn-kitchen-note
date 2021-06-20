af640c49b6b96a250d108b874dde5f31947b5cd7
tung47 <tung.nguyen271193@gmail.com>
2021-06-20 12:13:17 +0300

    fix shadows style

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -39,6 +39,12 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150,
+    borderRadius: 10,
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
+    elevation: 5,
   },
   touchableComponent: {
     flex: 1,
@@ -50,7 +56,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
