5538b8698337ab1b8c0e128f62de1fb96f74903c
tung47 <tung.nguyen271193@gmail.com>
2021-06-27 22:33:06 +0300

    render a fallback text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favoriteMeals.length === 0 || !favoriteMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 };
@@ -27,5 +37,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default FavoritesScreen;
